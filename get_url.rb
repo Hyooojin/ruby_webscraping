79034c5e32ff45bd54b9161c12b9b5c763202dab
jjuya <jjuya9381@naver.com>
2017-12-03 02:52:14 +0000

    update get_url.md

--- get_url.rb
+++ get_url.rb
@@ -3,19 +3,24 @@ require 'httparty'
 
 
 # (1) url을 입력받는다.
-# (2) urldptj com 앞에 부분을 받을 수 있도록! 
+# (2) url com 앞에 부분을 받을 수 있도록! 
 # (3) http:// 제거 .com 제거
 
 
 
-# url = "http://cafe.naver.com/jpnstory"
-#     # 1. '.'으로 나눠서 url을 나눈다.
-# url_re = url.to_s.split('.')    
-# puts url_re
+url = "http://cafe.naver.com/jpnstory"
+    # 1. '.'으로 나눠서 url을 나눈다.
+url_re = url.to_s.split(/\W+/)    
+puts url_re
+
 # regular expession
-value = "one,two three:four"
-a = value.split(/\W+/)
-puts a
+    # re 1
+    
+# value = "one,two three:four"
+# a = value.split(/\W+/)
+# puts a
+
+# one tow three four
 
 
 
